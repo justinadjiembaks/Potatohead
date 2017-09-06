14628329fa205291581e6ec8acff6d572d56f3df
justinadjiembaks <justin_adjiembaks@hotmail.com>
2017-09-06 21:49:41 +0200

    Checkbox insert

--- app/src/main/java/com/example/justi/potatohead/potatohead.java
+++ app/src/main/java/com/example/justi/potatohead/potatohead.java
@@ -2,6 +2,10 @@ package com.example.justi.potatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class potatohead extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class potatohead extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_potatohead);
-    }
+
+        String[] Face = {"Nose", "Eyes", "Eyebrows", "Hat", "Arms", "Shoes", "Ears", "Mustache"};
+        }
+
 }
