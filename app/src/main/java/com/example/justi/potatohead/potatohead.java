5c6c76ab86f627e0176c05f86799443d588fca18
justinadjiembaks <justin_adjiembaks@hotmail.com>
2017-09-06 23:48:21 +0200

    Checkbox linked to images

--- app/src/main/java/com/example/justi/potatohead/potatohead.java
+++ app/src/main/java/com/example/justi/potatohead/potatohead.java
@@ -1,36 +1,32 @@
 package com.example.justi.potatohead;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class potatohead extends AppCompatActivity {
+
+    ImageView hat, nose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_potatohead);
 
-        String[] Face = {"Nose", "Eyes", "Eyebrows", "Hat", "Arms", "Shoes", "Ears", "Mustache"};
-        }
-
-    boolean isChecked = ((CheckBox) findViewById(R.id.checkbox1)).isChecked();
-
-    public boolean isChecked() {
-        if (isChecked == true){
-
-            R.id.hat.setVisibility(View.VISIBLE);
-        }
-
-        else{
-            R.id.hat.
-
-        }
-        return isChecked;
+        hat = (ImageView)findViewById(R.id.hat);
+        nose = (ImageView) findViewById(R.id.nose);
     }
+    public void box1clicked(View v){
+        hat.setVisibility(View.VISIBLE);
 
+
+    }
 }
