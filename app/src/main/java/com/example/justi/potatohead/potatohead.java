488f00d5d4851495793541a28356dae0b2a5c265
justinadjiembaks <justin_adjiembaks@hotmail.com>
2017-09-06 12:26:57 +0200

    First Upload to Github

---
+++ app/src/main/java/com/example/justi/potatohead/potatohead.java
@@ -0,0 +1,13 @@
+package com.example.justi.potatohead;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class potatohead extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potatohead);
+    }
+}
